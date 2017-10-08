3c78c9e400878d7b8d81964a563e06a79bff7d22
KameriiJ <jenny.sirarat@gmail.com>
2017-10-08 17:59:58 +0700

    add array method

--- src/code/Restaurant.java
+++ src/code/Restaurant.java
@@ -1,127 +1,172 @@
 package code;
 
 import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Main class for SKE Restaurarnt order taking application.
+ * It displays a menu and accepts items to order.
+ * When done, it prints a receipt including the order total.
+ * Or playing the lucky promotions to give some allowance to customer.
+ *
+ * @author Gunthee Tawewatmingkol
+ */
 public class Restaurant {
-	static int choice = 0,qtt = 0,price = 0,sumQ = 0,sum = 0;
-	static int sum1,sum2,sum3,sumQ1,sumQ2,sumQ3,price1,price2,price3;
+	static Scanner myScan = new Scanner(System.in);
+	static String[] menu = {"Pizza","Chickens","Coke"};
+	static int[] allPrice = {250,120,45};
+	static int[] discountPercent = {0,5,10,25,50};
 	
-	public static int choiceChoose(int choicex,int qttx,int p){
-			price = qttx*p;
-			return price;
+	public static int getChoice(){
+		System.out.print("Enter your Choice : ");
+		int choice = myScan.nextInt();
+		return choice;
 	}
-	public static int priceEachMenu(int choicex,int pricex,int sumx){
-		sumx = sumx + pricex;
-		return sumx;
+	
+	public static int getQuantity(){
+		System.out.print("Enter Quantity : ");
+		int quantity = myScan.nextInt();
+		return quantity;
 	}
-	public static int qttPlus(int choicex,int qttx,int sumQx){
-			sumQx = sumQx + qttx;
-			return sumQx;
+	
+	public static double getTotal(int[] unitQuantity){
+		double total = 0.0;
+	    for(int x = 0;x < unitQuantity.length;x++) {
+	    	 total += unitQuantity[x]*allPrice[x];
+	    }
+	    return total;
 	}
-	public static void menuChoose(int choicex,int sumx1,int sumx2,int sumx3,int sumxQ1,int sumxQ2,int sumxQ3){
+	
+	public static int[] sumOfUnitPrice(int[] unitPrice,int[] unitQuantity){
+		for(int x = 0;x < menu.length;x++){
+			unitPrice[x] = allPrice[x]*unitQuantity[x];
+		}
+		return unitPrice;
+	}
+	
+	public static void menuChoose(int[] sumQuantity,int[] sumPrice,double total){
 		System.out.println("\n______MENU______ _____Qty_______ _____PRICE_____");
-		if(sumx1>=0){
-			System.out.println("| Pizza\t\t|       "+sumxQ1+"\t|"+"       "+sumx1+"  \t|");
+		if(sumQuantity[0]>=0){
+			System.out.printf("| %s\t\t|       %d\t|"+"       %d  \t|\n",menu[0],sumQuantity[0],sumPrice[0]);
 		}
-		if(sumx2>=0){
-			System.out.println("| Chickens\t|       "+sumxQ2+"\t|"+"       "+sumx2+"  \t|");
+		if(sumQuantity[1]>=0){
+			System.out.printf("| %s\t|       %d\t|"+"       %d  \t|\n",menu[1],sumQuantity[1],sumPrice[1]);
 		}
-		if(sumx3>=0){
-			System.out.println("| Coke\t\t|       "+sumxQ3+"\t|"+"       "+sumx3+"  \t|");
+		if(sumQuantity[2]>=0){
+			System.out.printf("| %s\t\t|       %d\t|"+"       %d  \t|\n",menu[2],sumQuantity[2],sumPrice[2]);
 		}
-		System.out.println    ("________________________________________________");
-		System.out.println  ("| Total\t\t\t|"+"\t\t"+(sumx1+sumx2+sumx3)+"\t|");
-		System.out.println    ("________________________________________________\n");
+		System.out.println("________________________________________________");
+		System.out.printf("| Total\t\t\t|"+"\t\t%.2f\t|\n",total);
+		System.out.println("________________________________________________\n");
 	}
-	public static void luckyPro6(int num){
+	
+	public static void chooseChoices(int choice,int[] sumQuantity,int[] sumPrice,double total){
+		if((choice>=1)&&(choice<=3)){
+			int quantity = getQuantity();
+			System.out.println();
+			if (choice == 1) {
+				sumQuantity[0] += quantity;
+			}
+			if (choice == 2) {
+				sumQuantity[1] += quantity;
+			}
+			if (choice == 3) {
+				sumQuantity[2] += quantity;
+			}
+		}
+		total = getTotal(sumQuantity);
+		int[] sumPrice1 = sumOfUnitPrice(sumPrice,sumQuantity);
+		if(choice==4){
+			menuChoose(sumQuantity,sumPrice1,total);
+		}
+		else if(choice==6){
+			System.out.println("...Your Total before play the game...");
+			menuChoose(sumQuantity,sumPrice1,total);
+			System.out.print("Please enter number 1,2 or 3 : ");
+			int luckNum1 = myScan.nextInt();
+			luckyPro6(luckNum1,choice,sumQuantity,sumPrice1);
+		}
+		else if(choice==7){
+			System.out.println("...Your Total before discount...");
+			menuChoose(sumQuantity,sumPrice1,total);
+			System.out.print("Please input your name : ");
+			String name = myScan.next();
+			luckyPro7(discountPercent,choice,sumQuantity,sumPrice1);
+		}
+	}
+	
+	public static void luckyPro6(int num,int choice,int[] sumQuantity,int[] sumPrice){
 		Random rand = new Random();
-		int ran1 = rand.nextInt(3)+1;
-		if(num==ran1){
+		int randomNumber = rand.nextInt(3)+1;
+		if(num==randomNumber){
 			System.out.println("CONGRATS!!!!YOU GET 'FREE' MEAL!!!!!..(*O*)b");
-			menuChoose(0,0,0,0,0,0,0);
+			for(int x = 0;x < menu.length;x++){
+				sumQuantity[x] = 0;
+				sumPrice[x] = 0;
+			}
+			double total = getTotal(sumQuantity);
+			menuChoose(sumQuantity,sumPrice,total);
 		}
 		else{
 			System.out.println("Sorry...You have to pay x2 for this meal...(ToT)");
-			menuChoose(choice,sum1*2,sum2*2,sum3*2,sumQ1,sumQ2,sumQ3);
+			double total = getTotal(sumQuantity)*2;
+			menuChoose(sumQuantity,sumPrice,total);
 		}
 	}
-	public static void luckyPro7(){
-		Random rand = new Random();
-		int ran1 = rand.nextInt(5)+0;
-		int ran2 = rand.nextInt(2)+1;
-		int r = ran2*5;
-		int disc = r*ran1;
-		if(disc!=0){
-			System.out.print("Congrats!..You get "+disc+"% discount.(^_^)");
-			menuChoose(choice,(sum1*(100-disc))/100,(sum2*(100-disc))/100,(sum3*(100-disc))/100,sumQ1,sumQ2,sumQ3);
+	
+	public static void luckyPro7(int[] discountPercent,int choice,int[] sumQuantity,int[] sumPrice){
+		int randomDiscount = new Random().nextInt(discountPercent.length);
+		int discount = discountPercent[randomDiscount];
+		if(discount!=0){
+			System.out.print("Congrats!..You get "+discount+"% discount.(^_^)");
+			double total = (getTotal(sumQuantity)*(100-discount))/100;
+			menuChoose(sumQuantity,sumPrice,total);
+			
 		}
 		else{
 			System.out.print("Sorry...You get 0% discount.(ToT)");
-			menuChoose(choice,sum1,sum2,sum3,sumQ1,sumQ2,sumQ3);
+			for(int x = 0;x < menu.length;x++){
+				sumQuantity[x] = 0;
+				sumPrice[x] = 0;
+			}
+			double total = getTotal(sumQuantity);
+			menuChoose(sumQuantity,sumPrice,total);
 		}
 	}
-	public static void main(String[] args){
-		Scanner myScan = new Scanner(System.in);
+	
+	public static void makeOrder(){
+		int choice = 0,quantity = 0;
+		double total = 0;
+		int[] unitQuantity = new int[menu.length];
+		int[] unitPrice = new int[menu.length];
+		do {
+			choice = getChoice();
+			chooseChoices(choice,unitQuantity,unitPrice,total);
+			if((choice==6)||(choice==7)){
+				break;
+			}
+		 } while(choice!= 5);
+		System.out.print("Your comments/suggestion : ");
+		String comment = myScan.next();
+		System.out.println("===== Thank you =====");
+	}
+	
+	public static void intro(String[] menu,int[] price){
 		System.out.print("---------Welcome to SKE Restaurant---------\n");
-		System.out.println("1.) Pizza\t250  Baht.");
-		System.out.println("2.) Chickens\t120  Baht.");
-		System.out.println("3.) Coke\t45   Baht.");
+		System.out.printf("1.) %s\t%d  %s.\n",menu[0],price[0],"Baht");
+		System.out.printf("2.) %s\t%d  %s.\n",menu[1],price[1],"Baht");
+		System.out.printf("3.) %s\t%d   %s.\n",menu[2],price[2],"Baht");
 		System.out.println("4.) Total");
 		System.out.println("5.) Exit\n");
 		System.out.println("--------- ::Lucky Promotion:: ---------");
 		System.out.println("**If you choose 'Lucky Promotion',you can't order more food or drink**");
 		System.out.println("6.) Free or x2");
 		System.out.println("7.) Get discount!! >> 0% to 50% <<\n");
-		
-		while(choice!=5){
-			System.out.print("Enter your Choice : ");
-			choice = myScan.nextInt();
-		
-			if(choice==4){
-				menuChoose(choice,sum1,sum2,sum3,sumQ1,sumQ2,sumQ3);
-				}
-			else if(choice==6){
-				System.out.print("Please enter number 1,2 or 3 : ");
-				int luckNum1 = myScan.nextInt();
-				luckyPro6(luckNum1);
-				break;
-			}
-			else if(choice==7){
-				System.out.println("...Your Total before discount...");
-				menuChoose(choice,sum1,sum2,sum3,sumQ1,sumQ2,sumQ3);
-				System.out.print("Please input your name : ");
-				String name = myScan.next();
-				luckyPro7();
-				break;
-			}
-			else if(choice==5){
-				break;
-			}
-			else{
-			System.out.print("Enter Quantity : ");
-			qtt = myScan.nextInt();
-			System.out.println();
-			
-			if(choice==1){
-				price1 = choiceChoose(choice,qtt,250);
-				sum1 = priceEachMenu(choice,price1,sum1);
-				sumQ1 = qttPlus(choice,qtt,sumQ1);
-			}
-			if(choice==2){
-				price2 = choiceChoose(choice,qtt,120);
-				sum2 = priceEachMenu(choice,price2,sum2);
-				sumQ2 = qttPlus(choice,qtt,sumQ2);
-			}
-			if(choice==3){
-				price3 = choiceChoose(choice,qtt,45);
-				sum3 = priceEachMenu(choice,price3,sum3);
-				sumQ3 = qttPlus(choice,qtt,sumQ3);
-			}
-			}
-		}
-		System.out.print("Your comments/suggestion : ");
-		String comment = myScan.next();
-		System.out.println("===== Thank you =====");
+	}
+	
+	public static void main(String[] args){
+		intro(menu,allPrice);
+		makeOrder();
 	}
 }
